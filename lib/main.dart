e8fbae3195cf224fe9f3d2564de0249bbde0ca8a
Kelvin Nyagudi <kelvinnyagudi@yahoo.com>
2024-05-17 22:52:59 +0300

    added categories and electronic images

--- lib/main.dart
+++ lib/main.dart
@@ -1,102 +1,86 @@
-// Importing the material package which contains the necessary widgets and APIs for building material design apps
 import 'package:flutter/material.dart';
 
-// The main function, entry point of the application
 void main() {
-  // Running the app by passing the instance of MyApp widget
   runApp(const MyApp());
 }
 
-// MyApp class, a StatelessWidget that represents the root of the application
 class MyApp extends StatelessWidget {
-  // Constructor for MyApp
   const MyApp({Key? key}) : super(key: key);
 
-  // The build method required to override for StatelessWidget, it returns the root widget of the application
   @override
   Widget build(BuildContext context) {
-    // MaterialApp widget, provides the core functionality of a material design app
     return MaterialApp(
-      // Title of the application
       title: 'Shopeasy',
-      // Theme data for the application, specifying the primary swatch color
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      // Setting debugShowCheckedModeBanner to false removes the debug banner in the app
       debugShowCheckedModeBanner: false,
-      // The home page of the application, an instance of MyHomePage widget
-      home: const MyHomePage(title: 'Shopeasy Home'),
+      home: const MyHomePage(title: 'ShopEasy Homepage'),
     );
   }
 }
 
-// MyHomePage class, a StatelessWidget representing the home page of the application
 class MyHomePage extends StatelessWidget {
-  // Title of the home page
   final String title;
 
-  // Constructor for MyHomePage
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // The build method required to override for StatelessWidget, it returns the widget hierarchy of the home page
   @override
   Widget build(BuildContext context) {
-    // Dummy list of categories
-    List<String> categories = [
-      'Electronics',
-      'Clothing',
-      'Shoes',
-      'Books',
-      'Home & Kitchen',
-      'Toys',
-      
-    ];
+    Map<String, String> categoryImages = {
+      'Electronics': 'assets/electronics.jpg',
+      'Clothing': 'assets/clothing.jpg',
+      'Shoes': 'assets/shoes.jpg',
+      'Books': 'assets/books2.jpg',
+      'Home & Kitchen': 'assets/kitchen_appliances2.jpg',
+      'Toys': 'assets/toys.jpg',
+    };
 
-    // Scaffold widget, provides a framework for implementing the basic material design visual layout structure of the home page
+    List<String> categories = categoryImages.keys.toList();
+
     return Scaffold(
-      // AppBar widget, represents the app bar at the top of the screen
       appBar: AppBar(
-        // Title of the app bar, centered using the Center widget
         title: Center(child: Text(title)),
       ),
-      // GridView.builder widget, builds a grid view with a custom builder
       body: GridView.builder(
-        // SliverGridDelegate that defines the grid layout
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2, // Number of columns in the grid
-          crossAxisSpacing: 10.0, // Spacing between columns
-          mainAxisSpacing: 10.0, // Spacing between rows
-          childAspectRatio: 2.0, // Aspect ratio of each tile (width / height)
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10.0,
+          mainAxisSpacing: 10.0,
+          childAspectRatio: 2.0,
         ),
-        // Number of items in the grid view
         itemCount: categories.length,
-        // Builder function that returns the widget for each item in the grid view
         itemBuilder: (BuildContext context, int index) {
-          // GestureDetector widget, detects gestures on its child and calls onTap callback when tapped
           return GestureDetector(
-            // onTap callback function called when the category tile is tapped
             onTap: () {
-              // Handle category tap by printing the tapped category
               print('Category tapped: ${categories[index]}');
-              // Navigate to categories screen with selected category
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (context) => CategoriesScreen(category: categories[index]),
+                  builder: (context) => CategoriesScreen(
+                    category: categories[index],
+                  ),
                 ),
               );
             },
-            // Card widget, represents a material design card containing the category name
             child: Card(
               elevation: 3.0,
-              // Centering the category name text within the card
-              child: Center(
-                // Text widget displaying the category name
-                child: Text(
-                  categories[index],
-                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-                ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    categoryImages[categories[index]]!,
+                    height: 80,
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    categories[index],
+                    style: const TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
               ),
             ),
           );
@@ -106,65 +90,115 @@ class MyHomePage extends StatelessWidget {
   }
 }
 
-// CategoriesScreen class, a StatelessWidget representing the screen displaying products for a specific category
 class CategoriesScreen extends StatelessWidget {
-  // Category for which the products are displayed
   final String category;
 
-  // Constructor for CategoriesScreen
   const CategoriesScreen({Key? key, required this.category}) : super(key: key);
 
-  // The build method required to override for StatelessWidget, it returns the widget hierarchy of the categories screen
   @override
   Widget build(BuildContext context) {
-    // Dummy list of products for the selected category
-    List<String> products = [
-      'Product 1',
-      'Product 2',
-      'Product 3',
-      'Product 4',
-      'Product 5',
-    ];
+    List<Map<String, dynamic>> products = [];
 
-    // Scaffold widget, provides a framework for implementing the basic material design visual layout structure of the categories screen
+    if (category == 'Electronics') {
+      products = [
+        {'name': 'TV', 'price': '\$10', 'quantity': 5, 'image': 'assets/tv.jpg'},
+        {'name': 'Hi-Fi', 'price': '\$20', 'quantity': 3, 'image': 'assets/hifi.jpg'},
+        {'name': 'Laptop', 'price': '\$15', 'quantity': 7, 'image': 'assets/laptop.jpg'},
+        {'name': 'Mobile Phone', 'price': '\$25', 'quantity': 2, 'image': 'assets/mobile.jpg'},
+        {'name': 'Digital Camera', 'price': '\$30', 'quantity': 4, 'image': 'assets/digicam.jpg'},
+        ];
+     } else if (category == 'Clothing') {
+      products = [
+         {'name': 'T-Shirt', 'price': '\$15', 'quantity': 10, 'image': 'assets/tshirt.jpg'},
+        {'name': 'Jeans', 'price': '\$25', 'quantity': 8, 'image': 'assets/jeans.jpg'},
+        {'name': 'Dress', 'price': '\$30', 'quantity': 6, 'image': 'assets/dress.jpg'},
+        {'name': 'Jacket', 'price': '\$40', 'quantity': 5, 'image': 'assets/jacket.jpg'},
+      ];
+    } else if (category == 'Shoes') {
+      products = [
+        {'name': 'Sneakers', 'price': '\$50', 'quantity': 12, 'image': 'assets/sneakers.jpg'},
+        {'name': 'Boots', 'price': '\$60', 'quantity': 7, 'image': 'assets/boots.jpg'},
+        {'name': 'Sandals', 'price': '\$35', 'quantity': 9, 'image': 'assets/sandals.jpg'},
+        {'name': 'Heels', 'price': '\$45', 'quantity': 6, 'image': 'assets/heels.jpg'},
+      ];
+    } else if (category == 'Home & Kitchen') {
+      products = [
+        {'name': 'Fridge', 'price': '\$10', 'quantity': 15, 'image': 'assets/fridge.jpg'},
+        {'name': 'Nicrowave', 'price': '\$12', 'quantity': 10, 'image': 'assets/microwave.jpg'},
+        {'name': 'Cooker', 'price': '\$8', 'quantity': 20, 'image': 'assets/cooker.jpg'},
+        {'name': 'washing Machine', 'price': '\$15', 'quantity': 8, 'image': 'assets/washing_machine.jpg'},
+      ];
+    } else if (category == 'Books') {
+      products = [
+        {'name': 'Novel', 'price': '\$10', 'quantity': 15, 'image': 'assets/novel.jpg'},
+        {'name': 'Biography', 'price': '\$12', 'quantity': 10, 'image': 'assets/biography.jpg'},
+        {'name': 'Self-Help', 'price': '\$8', 'quantity': 20, 'image': 'assets/selfhelp.jpg'},
+        {'name': 'Cookbook', 'price': '\$15', 'quantity': 8, 'image': 'assets/cookbook.jpg'},
+      ];
+    } else if (category == 'Toys') {
+      products = [
+        {'name': 'Action Figure', 'price': '\$20', 'quantity': 25, 'image': 'assets/action_figure.jpg'},
+        {'name': 'Doll', 'price': '\$18', 'quantity': 30, 'image': 'assets/doll.jpg'},
+        {'name': 'LEGO Set', 'price': '\$30', 'quantity': 20, 'image': 'assets/lego.jpg'},
+        {'name': 'Puzzle', 'price': '\$12', 'quantity': 15, 'image': 'assets/puzzle.jpg'},
+      ];
+    }
+
     return Scaffold(
-      // AppBar widget, represents the app bar at the top of the screen
       appBar: AppBar(
-        // Title of the app bar, centered using the Center widget
         title: Text(category),
         centerTitle: true,
       ),
-      // GridView.builder widget, builds a grid view with a custom builder
       body: GridView.builder(
-        // SliverGridDelegate that defines the grid layout
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2, // Number of columns in the grid
-          crossAxisSpacing: 10.0, // Spacing between columns
-          mainAxisSpacing: 10.0, // Spacing between rows
-          childAspectRatio: 1.5, // Aspect ratio of each tile (width / height)
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10.0,
+          mainAxisSpacing: 10.0,
+          childAspectRatio: 1.5,
         ),
-        // Number of items in the grid view
         itemCount: products.length,
-        // Builder function that returns the widget for each item in the grid view
         itemBuilder: (BuildContext context, int index) {
-          // GestureDetector widget, detects gestures on its child and calls onTap callback when tapped
           return GestureDetector(
-            // onTap callback function called when the product tile is tapped
             onTap: () {
-              // Handle product tap by printing the tapped product
-              print('Product tapped: ${products[index]}');
+              print('Product tapped: ${products[index]['name']}');
             },
-            // Card widget, represents a material design card containing the product name
             child: Card(
               elevation: 3.0,
-              // Padding around the product name text within the card
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                // Text widget displaying the product name
-                child: Text(
-                  products[index],
-                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-                ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image.asset(
+                    products[index]['image'],
+                    height: 80,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          products[index]['name'],
+                          style: const TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          'Price: ${products[index]['price']}',
+                          style: const TextStyle(
+                            fontSize: 14.0,
+                          ),
+                        ),
+                        Text(
+                          'Quantity: ${products[index]['quantity']}',
+                          style: const TextStyle(
+                            fontSize: 14.0,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
           );
@@ -173,3 +207,183 @@ class CategoriesScreen extends StatelessWidget {
     );
   }
 }
+
+
+
+
+
+
+
+
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Shopeasy',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       debugShowCheckedModeBanner: false,
+//       home: const MyHomePage(title: 'Shopeasy Home'),
+//     );
+//   }
+// }
+
+// class MyHomePage extends StatelessWidget {
+//   final String title;
+
+//   const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     Map<String, String> categoryImages = {
+//       'Electronics': 'assets/electronics.jpg',
+//       'Clothing': 'assets/clothing.jpg',
+//       'Shoes': 'assets/shoes.jpg',
+//       'Books': 'assets/books2.jpg',
+//       'Home & Kitchen': 'assets/kitchen_appliances2.jpg',
+//       'Toys': 'assets/toys.jpg',
+//     };
+
+//     List<String> categories = categoryImages.keys.toList();
+
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Center(child: Text(title)),
+//       ),
+//       body: GridView.builder(
+//         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+//           crossAxisCount: 2,
+//           crossAxisSpacing: 10.0,
+//           mainAxisSpacing: 10.0,
+//           childAspectRatio: 2.0,
+//         ),
+//         itemCount: categories.length,
+//         itemBuilder: (BuildContext context, int index) {
+//           return GestureDetector(
+//             onTap: () {
+//               print('Category tapped: ${categories[index]}');
+//               Navigator.push(
+//                 context,
+//                 MaterialPageRoute(
+//                   builder: (context) => CategoriesScreen(
+//                     category: categories[index],
+//                   ),
+//                 ),
+//               );
+//             },
+//             child: Card(
+//               elevation: 3.0,
+//               child: Column(
+//                 mainAxisAlignment: MainAxisAlignment.center,
+//                 children: [
+//                   Image.asset(
+//                     categoryImages[categories[index]]!,
+//                     height: 80,
+//                   ),
+//                   const SizedBox(height: 10),
+//                   Text(
+//                     categories[index],
+//                     style: const TextStyle(
+//                       fontSize: 18.0,
+//                       fontWeight: FontWeight.bold,
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           );
+//         },
+//       ),
+//     );
+//   }
+// }
+
+// class CategoriesScreen extends StatelessWidget {
+//   final String category;
+
+//   const CategoriesScreen({Key? key, required this.category}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     List<Map<String, dynamic>> products = [
+//       {'name': 'TV', 'price': '\$10', 'quantity': 5, 'image': 'assets/tv.jpg'},
+//       {'name': 'Hi-Fi', 'price': '\$20', 'quantity': 3, 'image': 'assets/hifi.jpg'},
+//       {'name': 'Laptop', 'price': '\$15', 'quantity': 7, 'image': 'assets/laptop.jpg'},
+//       {'name': 'Mobile Phone', 'price': '\$25', 'quantity': 2, 'image': 'assets/mobile.jpg'},
+//       {'name': 'Digital Camera', 'price': '\$30', 'quantity': 4, 'image': 'assets/digicam.jpg'},
+//     ];
+
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(category),
+//         centerTitle: true,
+//       ),
+//       body: GridView.builder(
+//         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+//           crossAxisCount: 2,
+//           crossAxisSpacing: 10.0,
+//           mainAxisSpacing: 10.0,
+//           childAspectRatio: 1.5,
+//         ),
+//         itemCount: products.length,
+//         itemBuilder: (BuildContext context, int index) {
+//           return GestureDetector(
+//             onTap: () {
+//               print('Product tapped: ${products[index]['name']}');
+//             },
+//             child: Card(
+//               elevation: 3.0,
+//               child: Column(
+//                 crossAxisAlignment: CrossAxisAlignment.start,
+//                 children: [
+//                   Image.asset(
+//                     products[index]['image'],
+//                     height: 80,
+//                   ),
+//                   Padding(
+//                     padding: const EdgeInsets.all(8.0),
+//                     child: Column(
+//                       crossAxisAlignment: CrossAxisAlignment.start,
+//                       children: [
+//                         Text(
+//                           products[index]['name'],
+//                           style: const TextStyle(
+//                             fontSize: 16.0,
+//                             fontWeight: FontWeight.bold,
+//                           ),
+//                         ),
+//                         Text(
+//                           'Price: ${products[index]['price']}',
+//                           style: const TextStyle(
+//                             fontSize: 14.0,
+//                           ),
+//                         ),
+//                         Text(
+//                           'Quantity: ${products[index]['quantity']}',
+//                           style: const TextStyle(
+//                             fontSize: 14.0,
+//                           ),
+//                         ),
+//                       ],
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           );
+//         },
+//       ),
+//     );
+//   }
+// }
+
+
